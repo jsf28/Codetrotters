e7b4ad8a83a04f4b236bfeabd4536db740fe6822
Jadec <jadec_soler@hotmail.com>
2015-10-22 20:49:55 -0400

    Changed content file

--- Intro_Backend/tableofcontents.rb
+++ Intro_Backend/tableofcontents.rb
@@ -4,3 +4,10 @@ puts "=" * 66
 puts "Chapter 1: Get Started".ljust(line_width) + "Page 1".ljust(line_width)
 puts "Chapter 2: All abouts cats".ljust(line_width) + "Page 3".ljust(line_width)
 puts "Chapter 3: The parts of a bicycle".ljust(line_width) + "Page 4".ljust(line_width)
+
+line_width = 80
+puts "Table of Contents".center(line_width)
+puts "=" * line_width
+puts "Chapter 1: Get Started".ljust(line_width/2) + "Page 1".rjust(line_width/2)
+puts "Chapter 2: All abouts cats".ljust(line_width/2) + "Page 3".rjust(line_width/2)
+puts "Chapter 3: The parts of a bicycle".ljust(line_width/2) + "Page 4".rjust(line_width/2)
