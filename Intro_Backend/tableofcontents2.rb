3c137a2c3e392c3de5ee9f40f9a93d809d9d3eb5
Jadec <jadec_soler@hotmail.com>
2015-11-01 17:01:16 -0400

    Added new file

---
+++ Intro_Backend/tableofcontents2.rb
@@ -0,0 +1,10 @@
+line_width = 60
+puts "Table of Contents 2".center(line_width)
+puts "=" * line_width
+puts "Write the title or the page than your looking for"
+answer = gets.chomp
+if answer == "get started" or "1"
+  puts "Chapter 1"
+else
+  puts "Rood"
+end
